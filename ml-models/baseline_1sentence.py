6a53563cc951e20cf1673c6daf7ee5bf004393a8
Tushar Abhishek <tushar.abhishek@ymail.com>
2019-10-21 01:14:23 +0530

    change from lex-rank to text-rank summarizer

--- ml-models/baseline_1sentence.py
+++ ml-models/baseline_1sentence.py
@@ -3,13 +3,13 @@ from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.summarizers.text_rank import TextRankSummarizer
 
 # from sumy.summarizers.lsa import LsaSummarizer
 # from sumy.summarizers.luhn import LuhnSummarizer
 # from sumy.summarizers.edmundson import EdmundsonSummarizer
 # from sumy.summarizers.kl import KLSummarizer
-# from sumy.summarizers.text_rank import TextRankSummarizer
+#from sumy.summarizers.lex_rank import LexRankSummarizer 
 # from sumy.summarizers.sum_basic import SumBasicSummarizer
 # from sumy.summarizers.reduction import ReductionSummarizer
 from sumy.nlp.stemmers import Stemmer
@@ -25,8 +25,8 @@ LANGUAGE = "english"
 # summarizers_name_list=['lex_rank','luhn','lsa','kl','text_rank','sum_basic','reduction']
 # summarizers_list=[LsaSummarizer,LexRankSummarizer,LuhnSummarizer,KLSummarizer,TextRankSummarizer,SumBasicSummarizer,ReductionSummarizer]
 
-summarizers_name_list=['lex_rank']
-summarizers_list=[LexRankSummarizer]
+summarizers_name_list=['text_rank']
+summarizers_list=[TextRankSummarizer]
 SENTENCES_COUNT = 1
 
 for hnd in handles:
